fee37e69d8649f16d800516ca3c515a751dc5cbf
Federico <federico.izzop@gmail.com>
2020-10-23 14:27:50 +0200

    first try

--- sketch.js
+++ sketch.js
@@ -1,12 +1,34 @@
+let do1;
+let re1;
+let mi1;
+let fa1;
+let sol1;
+let la1;
+let si1
+
 function preload(){
-  // put preload code here
-}
+  do1 = loadSound("assets/do.mp3");
+  re1 = loadSound("assets/re.mp3");
+  mi1 = loadSound("assets/mi.mp3");
+  fa1 = loadSound("assets/fa.mp3");
+  sol1 = loadSound("assets/sol.mp3");
+  la1 = loadSound("assets/la.mp3");
+  si1 = loadSound("assets/si.mp3");
 
+
+}
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
 }
-
 function draw() {
-  // put drawing code here
+  if (mouseIsPressed) {
+    background(0,255,0);
+    if (do1.isPlaying() == false) {
+      do1.play();
+    }
+  } else {
+    background(255,0,0);
+    do1.stop();
+
+  }
 }
